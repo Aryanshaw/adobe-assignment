2994484d3fa485d1c92db6e91818af7dead70a11
aryanShaw <aryanrot234@gmail.com>
2026-03-21 23:30:02 +0530

    removing unused redis connection

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ from fastapi import FastAPI
 from langchain_core.globals import set_verbose, set_debug
 from app.logger import logger
 from app.db.qdrant import qdrant_db
-from app.db.redis import redis_db
 from app.db.bm25 import bm25_db
 from app.db.sqlite import sqlite_db
 from app.db.migrate import run_migrations
@@ -29,7 +28,6 @@ async def lifespan(app: FastAPI):
     logger.info("Global LangChain Verbose/Debug modes enabled")
 
     await qdrant_db.connect()
-    await redis_db.connect()
     await bm25_db.connect()
     await sqlite_db.connect()
     logger.info("connections initialized")
@@ -40,7 +38,6 @@ async def lifespan(app: FastAPI):
     yield
 
     await qdrant_db.disconnect()
-    await redis_db.disconnect()
     await bm25_db.disconnect()
     await sqlite_db.disconnect()
     logger.info("connections disconnected")
